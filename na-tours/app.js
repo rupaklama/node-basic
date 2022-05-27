316427f6caf4776a0e2a12bc3f861900f486f3ae
pak <rupaklamadev@gmail.com>
2022-05-27 16:53:06 -0400

    get and post requests

--- na-tours/app.js
+++ na-tours/app.js
@@ -1,17 +1,72 @@
+const fs = require('fs');
 const express = require('express');
 
 /* express app instance */
 const app = express();
 
+// Middleware to consume Request Body Object
+app.use(express.json());
+
 /* routes */
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+/* File Reading should be done outside of Route handler */
+// __dirname - current root folder
+const toursData = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+/* Keeping Api endpoints exactly the SAME as a good practice with related Route handler */
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    // sending additional data for client side
+    results: toursData.length,
+    data: {
+      // matching endpoint
+      tours: toursData,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+app.post('/api/v1/tours', (req, res) => {
+  // Express does not put Body data/object in the request by default
+  // We have to use Middleware to have Request Body Object available
+  // console.log(req.body); NOTE - the Request Body gets parsed into an Object
+
+  // note - accessing object's id property & adding 1 to create a new id
+  const newId = toursData[toursData.length - 1].id + 1;
+  const newTour = { id: newId, ...req.body }; // or Object.assign({id: newId}, req.body)
+
+  toursData.push(newTour);
+
+  // NOTE - now this Route handler runs inside of the event loop & we should not block the event loop
+  // therefore, we are going to use the Async write func and not the Synchronous one
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+
+    // note - parsing toursData object into JSON Strings since our local data is json
+    JSON.stringify(toursData),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+
+  // Note - we always need to send back something to finish Request/Response Cycle
+  // res.send('Done');
 });
 
 const port = 4000;
